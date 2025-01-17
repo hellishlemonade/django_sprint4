c6b1c219e5e40cf8e2b4ada844467a81823d7dbc
Dmitriy Lyapin <hellishlemonade111@gmail.com>
2025-01-18 01:48:30 +0300

    for review

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,13 +1,14 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import get_object_or_404, redirect
 from django.views.generic import (
     CreateView, DeleteView, DetailView, ListView, UpdateView
 )
 from django.urls import reverse_lazy, reverse
-from django.contrib.auth import get_user_model
-from django.contrib.auth.views import LoginView
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.contrib.auth.forms import UserCreationForm
 from django.http import Http404
+
 
 from .models import Post, Category, Comment
 from .forms import PostForm, CommentForms
@@ -28,8 +29,6 @@ class OnlyAuthorMixin(UserPassesTestMixin):
         if self.request.user.is_authenticated:
             object = self.get_object()
             return object.author == self.request.user
-        else:
-            return None
 
     def handle_no_permission(self):
         return redirect('blog:post_detail', pk=self.kwargs['pk'])
@@ -37,12 +36,14 @@ class OnlyAuthorMixin(UserPassesTestMixin):
 
 class OnlyUserMixin(UserPassesTestMixin):
 
+    def get_object(self):
+        obj = get_object_or_404(User, username=self.kwargs['username'])
+        return obj
+
     def test_func(self):
         if self.request.user.is_authenticated:
             object = self.get_object()
             return object.username == self.request.user.username
-        else:
-            return None
 
 
 class ListViewMixin:
@@ -91,6 +92,8 @@ class CustomProfileLogin(SuccessReverseProfile, LoginView):
 
 
 class PostListView(ListViewMixin, ListView):
+    """Класс, отображающий главную страницу проекта с постами"""
+
     template_name = 'blog/index.html'
     queryset = Post.objects.filter(
         pub_date__lt=date_now(),
@@ -103,6 +106,8 @@ class PostListView(ListViewMixin, ListView):
 
 
 class CategoryListView(ListViewMixin, ListView):
+    """Класс, отображающий отдельную страницу с выбранной категорией"""
+
     template_name = 'blog/category.html'
 
     def get_context_data(self, **kwargs):
@@ -129,6 +134,8 @@ class CategoryListView(ListViewMixin, ListView):
 class PostCreateView(
     LoginRequiredMixin, PostFormMixin, SuccessReverseProfile, CreateView
 ):
+    """Класс, обрабатывающий страницу создания поста"""
+
     template_name = 'blog/create.html'
 
     def form_valid(self, form):
@@ -137,6 +144,8 @@ class PostCreateView(
 
 
 class PostDetailView(DetailView):
+    """Класс, отображающий страницу отдельного поста"""
+
     model = Post
     template_name = 'blog/detail.html'
 
@@ -154,15 +163,19 @@ class PostDetailView(DetailView):
         context['post'] = post
         context['comments'] = (
             Comment.objects.filter(post=post).order_by('created_at')
-        ).select_related('author')
+        ).select_related('author', 'post')
         return context
 
 
 class PostUpdateView(OnlyAuthorMixin, PostFormMixin, UpdateView):
+    """Класс, который позволяет редактировать пост"""
+
     template_name = 'blog/create.html'
 
 
 class PostDeleteView(OnlyAuthorMixin, SuccessReverse, DeleteView):
+    """Класс, управляющий удалением постов"""
+
     model = Post
 
     def get_object(self):
@@ -174,53 +187,84 @@ class PostDeleteView(OnlyAuthorMixin, SuccessReverse, DeleteView):
 
 
 class ProfileListView(ListView):
+    """Класс, отображающий страницу профиля"""
+
     model = User
     template_name = 'blog/profile.html'
     paginate_by = 10
 
     def get_user(self):
-        if self.request.user.is_authenticated:
-            return get_object_or_404(User, username=self.kwargs['username'])
-        else:
-            return None
+        return get_object_or_404(User, username=self.kwargs['username'])
 
     def get_context_data(self, **kwargs):
         user = self.get_user()
         context = super(ProfileListView, self).get_context_data(**kwargs)
         context['profile'] = user
+        context['post'] = Post.objects.filter(author=user)
         return context
 
     def get_queryset(self):
         user = self.get_user()
         if self.request.user.is_authenticated:
             if self.request.user.username == user.username:
-                return Post.objects.filter(author=user)
+                return Post.objects.filter(
+                    author=user
+                ).select_related(
+                    'author', 'location', 'category'
+                ).prefetch_related('comments')
             else:
-                return Post.objects.filter(author=user, is_published=True, category__is_published=True)
+                return Post.objects.filter(
+                    author=user,
+                    is_published=True,
+                    category__is_published=True
+                ).select_related(
+                    'author', 'location', 'category'
+                ).prefetch_related('comments')
         else:
-            return Post.objects.filter(author=user, is_published=True, category__is_published=True)
-
+            return Post.objects.filter(
+                author=user,
+                is_published=True,
+                category__is_published=True
+            ).select_related(
+                'author', 'location', 'category'
+            ).prefetch_related('comments')
 
 
 class ProfileCreateView(SuccessReverse, CreateView):
+    """Класс, обрабатывающий создание профиля"""
+
     template_name = 'registration/registration_form.html'
     form_class = UserCreationForm
 
 
 class ProfileUpdateView(OnlyUserMixin, SuccessReverse, UpdateView):
+    """Класс, обрабатывающий редактирование профиля"""
+
     model = User
     template_name = 'blog/user.html'
     fields = ['username', 'first_name', 'last_name', 'email',]
 
+    def get_queryset(self):
+        user = self.get_object()
+        return user
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
 
 class CommentCreateView(LoginRequiredMixin, CreateView):
+    """Класс, обрабатывающий создание комментария"""
+
     object = None
     model = Comment
     form_class = CommentForms
 
     def dispatch(self, request, *args, **kwargs):
         self.object = get_object_or_404(Post, pk=kwargs['pk'])
-        return super(CommentCreateView, self).dispatch(request, *args, **kwargs)
+        return super(
+            CommentCreateView, self
+        ).dispatch(request, *args, **kwargs)
 
     def get_object(self):
         post_id = self.kwargs['pk']
@@ -236,10 +280,14 @@ class CommentCreateView(LoginRequiredMixin, CreateView):
 
 
 class CommentUpdateView(OnlyAuthorMixin, CommentMixin, UpdateView):
+    """Класс, обрабатывающий редактирование профиля"""
+
     fields = ['text',]
 
 
 class CommentDeleteView(OnlyAuthorMixin, DeleteView):
+    """Класс, обрабатывающий удаление комментария"""
+
     model = Comment
     template_name = 'blog/comment.html'
 
@@ -254,8 +302,9 @@ class CommentDeleteView(OnlyAuthorMixin, DeleteView):
     def get_context_data(self, **kwargs):
         obj = self.get_object()
         context = super().get_context_data(**kwargs)
-        if 'form' not in context:
-            context['comment'] = obj
+        context['comment'] = obj
+        if 'form' in context:
+            del context['form']
         return context
 
     def get_success_url(self):
